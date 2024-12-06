88f0f8d6274b45966e21bec4c9bcd843efea2efe
SorenR1537 <150212440+SorenR1537@users.noreply.github.com>
2024-12-05 23:14:12 -0500

    Subir proyecto inicial

---
+++ CL2-RIOS-SOREN-MVC/src/main/java/com/edu/cibertec/CL2_RIOS_SOREN_MVC/Cl2RiosSorenMvcApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.CL2_RIOS_SOREN_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2RiosSorenMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2RiosSorenMvcApplication.class, args);
+	}
+
+}
---
+++ CL2-RIOS-SOREN-MVC/src/test/java/com/edu/cibertec/CL2_RIOS_SOREN_MVC/Cl2RiosSorenMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cibertec.CL2_RIOS_SOREN_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2RiosSorenMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
